45f3eda0afa117268a7193d0581e07f80be8276a
Fernando González <rmfernando@outlook.com>
2020-04-23 16:15:13 -0600

    Fail

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express')
 var app = express()   // The code below will display 'Hello World!' to the browser when you go to http://localhost:3000 
 
 app.get('/', function (req, res) {
-    res.send('Hello World! This is a CI test.')
+    res.send('Hello World! This is a CI test')
 })
 
 app.listen(3000, function () {
